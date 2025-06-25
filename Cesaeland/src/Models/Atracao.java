09a955243a10b5cad0dab9ce3fda103576313341
franciscoperez10 <fmlperez95@gmail.com>
2025-06-25 12:35:58 +0100

    Alterações locais em SaleRepository

---
+++ Cesaeland/src/Models/Atracao.java
@@ -0,0 +1,38 @@
+package Models;
+
+public class Atracoes {
+
+    private int id;
+    private String atracao;
+    private double precoAdulto;
+    private double precoCrianca;
+    private int duracaoSegundos;
+
+    public Atracoes(int id, String atracao, double precoAdulto, double precoCrianca, int duracaoSegundos) {
+        this.id = id;
+        this.atracao = atracao;
+        this.precoAdulto = precoAdulto;
+        this.precoCrianca = precoCrianca;
+        this.duracaoSegundos = duracaoSegundos;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getAtracao() {
+        return atracao;
+    }
+
+    public double getPrecoAdulto() {
+        return precoAdulto;
+    }
+
+    public double getPrecoCrianca() {
+        return precoCrianca;
+    }
+
+    public int getDuracaoSegundos() {
+        return duracaoSegundos;
+    }
+}
